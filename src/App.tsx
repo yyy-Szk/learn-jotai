19efe10f01ebf64669711ffd36a658175b360b66
Yoshimasa Suzuki <cognac0004@gmail.com>
2025-03-20 16:43:58 +0900

    feat: ドットで絵を描く画面を表示できるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import "./App.css";
+import { useState } from "react";
+
 import { AnimeApp } from "./components/AnimeApp";
+import { SvgRoot } from "./components/SvgRoot";
+
+import "./App.css";
+
+type Page = "anime" | "svg";
 
 function App() {
-  return <AnimeApp />;
+  const [page, setPage] = useState<Page>("anime");
+
+  return (
+    <>
+      {page === "anime" && <AnimeApp />}
+      {page === "svg" && <SvgRoot />}
+
+      <button onClick={() => setPage("anime")}>Anime</button>
+      <button onClick={() => setPage("svg")}>SVG</button>
+    </>
+  );
 }
 
 export default App;
